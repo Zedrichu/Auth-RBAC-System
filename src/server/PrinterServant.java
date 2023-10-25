d102ca0e50671c0278a95409a56458c016eb6094
Zedrichu <adrzvizdencojr@gmail.com>
2023-10-25 15:50:01 +0200

    Added print statements in all printer methods

--- src/server/PrinterServant.java
+++ src/server/PrinterServant.java
@@ -12,40 +12,40 @@ public class PrinterServant extends UnicastRemoteObject implements IPrinterServi
 
     @Override
     public void print(String filename, String printer) throws RemoteException {
-        System.out.printf("Printed %s on printer %s", filename, printer);
+        System.out.printf("|> Called print(%s,%s)%n", filename, printer);
     }
 
     @Override
     public void queue(String printer) throws RemoteException {
-
+        System.out.printf("|> Called queue(%s)%n", printer);
     }
 
     @Override
     public void topQueue(String printer, int job) throws RemoteException {
-
+        System.out.printf("|> Called topQueue(%s, %d)%n", printer, job);
     }
 
     public void start() throws RemoteException {
-        System.out.println("Printer job started");
+        System.out.println("|> Printer job started");
     }
 
     @Override
     public void stop() throws RemoteException {
-        System.out.println("Printer job stopped");
+        System.out.println("|> Printer job stopped");
     }
 
     @Override
     public void status(String printer) throws RemoteException {
-        System.out.println("Printer is active");
+        System.out.printf("|> Printer '%s' is active%n", printer);
     }
 
     @Override
     public void readConfig(String parameter) throws RemoteException {
-
+        System.out.printf("|> Called readConfig(%s)%n", parameter);
     }
 
     @Override
     public void setConfig(String parameter, String value) throws RemoteException {
-
+        System.out.printf("|> Called setConfig(%s)%n", parameter);
     }
 }
