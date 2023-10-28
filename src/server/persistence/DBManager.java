de93ce56672e726ebca584688479574d38f6b8e7
arthurbosquetti <arthur.bosquetti@gmail.com>
2023-10-28 16:52:09 +0200

    Added support methods

--- src/server/persistence/DBManager.java
+++ src/server/persistence/DBManager.java
@@ -1,18 +1,17 @@
 package server.persistence;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBManager {
 
+    private final String url = "jdbc:h2:file:./data/db_file";
+    private final String user = "Group85";
+    private final String password = "";
+    private Connection connection;
+
     private static DBManager dbManager = null;
 
-    private double randomId;
-
-    private DBManager(){
-        randomId = Math.random();
-    }
+    private DBManager() {}
 
     public static DBManager getInstance() {
         if (dbManager == null) {
@@ -21,26 +20,30 @@ public class DBManager {
         return dbManager;
     }
 
-    public double getRandomId() {
-        return randomId;
-    }
-
-    public static void main(String[] args) {
-        String url = "jdbc:h2:file:./data/db_file";
-        String user = "Group85";
-        String password = "";
-
+    public boolean connect() {
         try {
             DriverManager.registerDriver(new org.h2.Driver());
-            Connection connection = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to the DB!");
-            connection.close();
+            return true;
         } catch (SQLException e) {
             System.out.println("Connection failed!");
             e.printStackTrace();
+            return false;
         }
-
     }
 
+    public ResultSet executeQuery(String query) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        resultSet.next();
+        return resultSet;
+    }
+
+    public int executeUpdate(String query) throws SQLException {
+        Statement statement = connection.createStatement();
+        int returnCode = statement.executeUpdate(query);
+        return returnCode;
+    }
 
 }
