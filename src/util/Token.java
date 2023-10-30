c8bde60bfc6e20757be8da060e156bce98ee1a01
Zedrichu <adrzvizdencojr@gmail.com>
2023-10-30 01:55:19 +0100

    Implemented CryptoHasher, fixed authentication to use cryptohasher for verification of digests
    Added SecureRandom in DBManager for generation of salts
    
    Used HashMap in TkManager instead of List
    
    Cleanup of AppServer, Service interfaces,registry binding,
    
    Small changes inToken and TokenManager

--- src/util/Token.java
+++ src/util/Token.java
@@ -5,22 +5,13 @@ import java.time.ZoneOffset;
 import java.util.UUID;
 
 public class Token {
-    public UUID token;
+    public UUID id;
     public String username;
     public LocalDateTime startTime;
 
     public Token(String username) {
-        this.token = UUID.randomUUID();
+        this.id = UUID.randomUUID();
         this.username = username;
         this.startTime = LocalDateTime.now(ZoneOffset.UTC);
     }
-
-    @Override
-    public boolean equals(Object object) {
-        if (object instanceof Token) {
-            return this.token == ((Token) object).token;
-        }
-        return false;
-    }
-
 }
