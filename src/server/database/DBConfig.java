9da2f8c45ef3c0a1c2e511dd310617c1672b1bd2
Zedrichu <adrzvizdencojr@gmail.com>
2024-12-27 00:16:40 +0200

    Reorganized database link

--- src/server/database/DBConfig.java
+++ src/server/database/DBConfig.java
@@ -16,7 +16,7 @@
 package server.database;
 
 public class DBConfig {
-    public static final String URL = "jdbc:h2:file:./database/db_file";
+    public static final String URL = "jdbc:h2:file:./data/db_file";
     public static final String USER = "Group85";
     public static final String PASSWORD = "";
 }
